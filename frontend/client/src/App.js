31cc3e20dbc6c9c85f181e7c86dd25e980567ded
thelonehegelian <thelonehegelian@gmail.com>
2020-09-06 10:16:56 +0100

    Add Submit function

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import Fibonacci from './contracts/Fibonacci.json';
-import { getWeb3 } from './utils.js';
+import React, { useEffect, useState } from "react";
+import Fibonacci from "./contracts/Fibonacci.json";
+import { getWeb3 } from "./utils.js";
 
 function App() {
   const [web3, setWeb3] = useState(undefined);
   const [contract, setContract] = useState(undefined);
+  const [result, setResult] = useState(undefined);
 
   useEffect(() => {
     const init = async () => {
@@ -14,13 +15,21 @@ function App() {
       const deployedNetwork = Fibonacci.networks[networkId];
       const contract = new web3.eth.Contract(
         Fibonacci.abi,
-        deployedNetwork && deployedNetwork.address,
+        deployedNetwork && deployedNetwork.address
       );
       setWeb3(web3);
       setContract(contract);
-    }
+    };
     init();
   }, []);
+
+  async function submit(evt) {
+    evt.preventDefault();
+    const num = evt.target.elements[0].value;
+    const result = await web3.eth.methods.fib(num);
+    setResult(result);
+  }
+
   if (!web3) {
     return <div>Loading...</div>;
   }
@@ -31,17 +40,21 @@ function App() {
 
       <div className="row">
         <div className="col-sm-12">
-          <form>
+          <form
+            onSubmit={(evt) => {
+              this.submit(evt);
+            }}>
             <div className="form-group">
               <label htmlFor="number">Fibonacci sequence of</label>
               <input type="number" className="form-control" id="number" />
             </div>
-            <button type="submit" className="btn btn-primary">Submit</button>
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
             <p>{result && `Result: ${result}`}</p>
           </form>
         </div>
       </div>
-
     </div>
   );
 }
